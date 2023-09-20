66832b16582403304d361ba1f0d21f008eb1e43c
Moamen Mostafa <mmoamen270@gmail.com>
2023-09-20 18:17:26 +0200

    Add Extention Application Services

---
+++ DSC/Extentions/ApplicationServiceExtentions.cs
@@ -0,0 +1,14 @@
+﻿namespace DSC.Extentions
+{
+    public static class ApplicationServiceExtentions
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.Configure<MailSetting>(configuration.GetSection("MailSettingsGmail"));
+            return services;
+        }
+    }
+}
