d94663099ef8aa27d10072a866799d4990d8d530
Moamen Mostafa <mmoamen270@gmail.com>
2023-09-20 18:20:15 +0200

    Add Repository

---
+++ DSC.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSC.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> AddAsync(T entity);
+
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+
+        T Update(T entity);
+
+        IEnumerable<T> UpdateRange(IEnumerable<T> entities);
+
+        T Delete(T entity);
+
+        public IEnumerable<T> DeleteRange(IEnumerable<T> entities);
+
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] includeProperties);
+
+        Task<IEnumerable<T>> GetAllNoTrackingAsync(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] includeProperties);
+
+        Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] includeProperties);
+
+        Task<int> CountAsync(Expression<Func<T, bool>> filter = null);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> filter = null);
+    }
+}
---
+++ DSC.DataAccess/Repository/Repository.cs
@@ -0,0 +1,95 @@
+﻿using DSC.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSC.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+            return entities;
+        }
+
+        public T Update(T entity)
+        {
+            _dbSet.Update(entity);
+            return entity;
+        }
+
+        public IEnumerable<T> UpdateRange(IEnumerable<T> entities)
+        {
+            _dbSet.UpdateRange(entities);
+            return entities;
+        }
+
+        public T Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            return entity;
+        }
+
+        public IEnumerable<T> DeleteRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+            return entities;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] includeProperties)
+        {
+            var query = filter != null ? _dbSet.Where(filter) : _dbSet;
+
+            if (includeProperties != null)
+                query = includeProperties.Aggregate(query, (record, property) => record.Include(property));
+            return await query.ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllNoTrackingAsync(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] includeProperties)
+        {
+            var query = filter != null ? _dbSet.Where(filter).AsNoTracking() : _dbSet;
+
+            if (includeProperties != null)
+                query = includeProperties.Aggregate(query, (record, property) => record.Include(property).AsNoTracking());
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] includeProperties)
+        {
+            var query = filter != null ? _dbSet.Where(filter) : _dbSet;
+
+            if (includeProperties != null)
+                query = includeProperties.Aggregate(query, (record, property) => record.Include(property));
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>> filter = null)
+        {
+            return await (filter != null ? _dbSet.CountAsync(filter) : _dbSet.CountAsync());
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> filter = null)
+        {
+            return await (filter != null ? _dbSet.AnyAsync(filter) : _dbSet.AnyAsync());
+        }
+    }
+}
