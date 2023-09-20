0a612ab0e2201f3a7e888406911f46556d9c4194
Moamen Mostafa <mmoamen270@gmail.com>
2023-09-20 18:21:06 +0200

    Add ApplicationDbContext with identity Customization

---
+++ DSC.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿namespace DSC.DataAccess;
+
+public class ApplicationDbContext : IdentityDbContext<User, Role, int,
+    IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>,
+    IdentityRoleClaim<int>, IdentityUserToken<int>>
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Role>().ToTable("Roles");
+        builder.Entity<User>().ToTable("Users");
+        builder.Entity<UserRole>().ToTable("UserRole");
+        builder.Entity<IdentityUserClaim<int>>().ToTable("UserClaims");
+        builder.Entity<IdentityUserLogin<int>>().ToTable("UserLogins");
+        builder.Entity<IdentityRoleClaim<int>>().ToTable("RoleClaims");
+        builder.Entity<IdentityUserToken<int>>().ToTable("UserTokens");
+
+        builder.Entity<User>()
+               .HasMany(ur => ur.UserRoles)
+               .WithOne(u => u.User)
+               .HasForeignKey(ur => ur.UserId)
+               .IsRequired();
+
+        builder.Entity<Role>()
+            .HasMany(ur => ur.UsersRole)
+            .WithOne(u => u.Role)
+            .HasForeignKey(ur => ur.RoleId)
+            .IsRequired();
+    }
+}
