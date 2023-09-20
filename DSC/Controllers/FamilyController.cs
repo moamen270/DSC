d0dadf2607ce5e7714e4b920ce4f585a2375fec2
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-09-20 19:37:44 +0300

    Added Controllers

---
+++ DSC/Controllers/FamilyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DSC.Controllers
+{
+    public class FamilyController : Controller
+    {
+
+
+        public FamilyController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ DSC/Controllers/ServiceController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DSC.Controllers
+{
+    public class ServiceController : Controller
+    {
+
+
+        public ServiceController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
