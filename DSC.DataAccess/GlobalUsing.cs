9ca813d5f96b4ede4eab81cc761c4105b8610546
Moamen Mostafa <mmoamen270@gmail.com>
2023-09-20 18:19:27 +0200

    Add Global Using

--- DSC.DataAccess/GlobalUsing.cs
+++ DSC.DataAccess/GlobalUsing.cs
@@ -1,1 +1,6 @@
-﻿
+﻿global using DSC.Models;
+global using Microsoft.EntityFrameworkCore;
+global using DSC.DataAccess.Repository.IRepository;
+global using System.Linq.Expressions;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
