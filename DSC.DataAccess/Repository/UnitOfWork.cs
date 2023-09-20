fd79a67040ddca17c6241362be02d5fadd15e4e7
Moamen Mostafa <mmoamen270@gmail.com>
2023-09-20 18:20:28 +0200

    Add Uinit of Work

---
+++ DSC.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using DSC.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSC.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task<int> Save()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
