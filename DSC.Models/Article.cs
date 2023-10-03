bfbed094d73114241d156e32a109f651b19c4fd0
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-10-03 21:47:23 +0300

    Finish FounderViews

--- DSC.Models/Article.cs
+++ DSC.Models/Article.cs
@@ -2,13 +2,13 @@
 {
     public class Article
     {
-        public int Id { get; set; }
-        public string Tittle { get; set; }
-        public DateTime CDate { get; set; }
-        public string Description { get; set; }
-        public string ImgUrl { get; set; }
-        public ArticleType ArticleType { get; set; }
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
+            public int Id { get; set; }
+            public string Tittle { get; set; }
+            public DateTime CDate { get; set; } = DateTime.Now;
+            public string Description { get; set; }
+            public string ImgUrl { get; set; }
+            public ArticleType ArticleType { get; set; }
+            public int CategoryId { get; set; }
+            public Category Category { get; set; }
     }
 }
