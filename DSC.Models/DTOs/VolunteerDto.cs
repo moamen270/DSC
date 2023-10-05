fa098b373dfbb16740f8faeaafb085ac1c095f93
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-10-05 18:00:47 +0300

    VolunteerController with his staff

--- DSC.Models/DTOs/VolunteerDto.cs
+++ DSC.Models/DTOs/VolunteerDto.cs
@@ -1,16 +1,18 @@
-﻿namespace DSC.Models
+﻿namespace DSC.Models.DTOs
 {
-	public class Volunteer
-	{
+    public class VolunteerDto
+    {
         public int Id { get; set; }
         public string Name { get; set; }
+
         public string PhoneNumber { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public string ImgUrl { get; set; }
         public string Description { get; set; }
+        public ICollection<SocialProfile> SocialProfiles { get; set; }
 
-		public ICollection<SocialProfile> SocialProfiles { get; set; }
+        public IEnumerable<Volunteer> Volunteers { get; set; }
 
-
-	}
+    }
 }
--- DSC/Controllers/VolunteerController.cs
+++ DSC/Controllers/VolunteerController.cs
@@ -1,21 +1,81 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DSC.Models;
+using DSC.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DSC.Controllers
 {
     public class VolunteerController : Controller
     {
-        public VolunteerController()
+        private readonly IUnitOfWork _unitOfWork;
+
+        public VolunteerController(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var volunteers = await _unitOfWork.Volunteer.GetAllAsync();
+            return View(new VolunteerDto { Volunteers = volunteers });
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
         {
-            return View();
+            var volunteer = await _unitOfWork.Volunteer.FirstOrDefaultAsync(v=>v.Id==id);
+            if (volunteer == null)
+                return NotFound();
+
+            return View(volunteer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Volunteer volunteer)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.Volunteer.AddAsync(volunteer);
+                await _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View(volunteer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Volunteer volunteer)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Volunteer.Update(volunteer);
+                await _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View(volunteer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSocialProfile(SocialProfile socialProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.SocialProfiles.AddAsync(socialProfile);
+                await _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View(socialProfile);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Volunteer volunteer)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Volunteer.Delete(volunteer);
+                await _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View(volunteer);
         }
     }
 }
