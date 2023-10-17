5209bbbb4c20bcc5300394fade90b930c3c9061d
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-10-17 23:21:43 +0300

    About View

---
+++ DSC/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DSC.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
