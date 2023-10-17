7464cfd58738ea9c9591dca8d04cde5323b272d3
Moamen Mostafa <mmoamen270@gmail.com>
2023-10-18 01:31:22 +0300

    Add Collection Dto

---
+++ DSC.Models/DTOs/CollectionDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace DSC.Models.DTOs
+{
+    public class CollectionDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public IEnumerable<Collection> Collections { get; set; }
+    }
+}
