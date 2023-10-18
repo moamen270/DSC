1c67c1b45d6b304a09cb7791ab27ee063b1b6256
Moamen Mostafa <mmoamen270@gmail.com>
2023-10-18 19:38:16 +0300

    Add media DTO

---
+++ DSC.Models/DTOs/MediaDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace DSC.Models.DTOs
+{
+    public class MediaDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public MediaType MediaType { get; set; }
+        public int CollectionId { get; set; }
+        public Collection Collection { get; set; }
+        public IFormFile File { get; set; }
+
+        public IEnumerable<Collection> Collections { get; set; }
+
+        public IEnumerable<Media> Medias { get; set; }
+    }
+}
