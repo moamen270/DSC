3ddeab4caf8d73fc3e269c2884ada6eed65d2075
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-10-19 00:46:29 +0300

    Add StudentForm with Controller

---
+++ DSC.Models/DTOs/StudentDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSC.Models.DTOs
+{
+    public class StudentDto
+    {
+        public string SSN { get; set; }
+        public string SsnImageUrl { get; set; }
+        public string StudentIdImageUrl { get; set; }
+        public string Address { get; set; }
+        public string ImageUrl { get; set; }
+        public string DisabilityType { get; set; }
+        public DateTime BithDate { get; set; }
+        public string Department { get; set; }
+        public CollageYear CollageYear { get; set; }
+        public Language Language { get; set; }
+        public StudyType StudyType { get; set; }
+        public Religion Religion { get; set; }
+        public Gender Gender { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<Enroll> Courses { get; set; }
+        public ICollection<Family> Family { get; set; }
+        public ICollection<Apply> Services { get; set; }
+        public Gender[] Genders { get; set; } = Enum.GetValues<Gender>();
+        public CollageYear[] CollageYears { get; set; } = Enum.GetValues<CollageYear>();
+        public Language[] Languages { get; set; } = Enum.GetValues<Language>();
+        public Religion[] Religions { get; set; } = Enum.GetValues<Religion>();
+        public StudyType[] StudyTypes { get; set; } = Enum.GetValues<StudyType>();
+    }
+}
---
+++ DSC/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿
+using DSC.Models.DTOs;
+
+
+namespace DSC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<User> _userManager;
+        public StudentController(IUnitOfWork unitOfWork, UserManager<User> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new StudentDto());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Student student)
+        {
+                var user = await _userManager.GetUserAsync(User);
+                student.UserId = user.Id;
+                await _unitOfWork.Student.AddAsync(student);
+                await _unitOfWork.Save();
+                return RedirectToAction("Index","Home");
+        }
+    }
+}
+
