5d63a0ff5a348fdae533e3b013fea167df4c004e
Moamen Mostafa <mmoamen270@gmail.com>
2023-10-18 18:57:10 +0300

    Add Course Info Controller

---
+++ DSC/Controllers/CourseInfoController.cs
@@ -0,0 +1,72 @@
+﻿using DSC.Models.DTOs;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSC.Controllers
+{
+    public class CourseInfoController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CourseInfoController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var courses = await _unitOfWork.Course.GetAllAsync();
+            var coursesInfo = await _unitOfWork.CourseInfo.GetAllAsync(includeProperties: property => property.Course);
+            return View(new CourseInfoDto { Courses = courses, CoursesInfo = coursesInfo });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int CourseInfoId)
+        {
+            var courseInfos = await _unitOfWork.CourseInfo.FirstOrDefaultAsync(course => course.Id == CourseInfoId, property => property.Course);
+            if (courseInfos == null)
+                return NotFound();
+            var topic = await _unitOfWork.Topic.FirstOrDefaultAsync(filter => filter.Id == courseInfos.Course.TopicId);
+
+            courseInfos.Course.Topic = topic;
+            return View(courseInfos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseInfo courseInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.CourseInfo.AddAsync(courseInfo);
+                await _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index", "Error");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CourseInfo courseInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CourseInfo.Update(courseInfo);
+                await _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index", "Error");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(CourseInfo courseInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CourseInfo.Delete(courseInfo);
+                await _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index", "Error");
+        }
+    }
+}
