ec779154ad678c6eb20fc9a0c4234d37d12d9622
Moamen Mostafa <mmoamen270@gmail.com>
2023-10-20 15:26:09 +0300

    Add Video Upload

---
+++ DSC.Services/IServices/IMediaService.cs
@@ -0,0 +1,19 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSC.Services.IServices
+{
+    public interface IMediaService
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+
+        Task<VideoUploadResult> AddVideoAsync(IFormFile file);
+
+        Task<DeletionResult> DeleteMediaAsync(string publicId);
+    }
+}
---
+++ DSC.Services/MediaService.cs
@@ -0,0 +1,82 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using DSC.Services.IServices;
+using DSC.Services.Setting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSC.Services
+{
+    public class MediaService : IMediaService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public MediaService(IOptions<CloudinarySettings> config)
+        {
+            var acc = new Account
+            (
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(acc);
+        }
+
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+            var uploadResult = new ImageUploadResult();
+
+            if (file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face"),
+                    Folder = "da-net7"
+                };
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+
+            return uploadResult;
+        }
+
+        public async Task<VideoUploadResult> AddVideoAsync(IFormFile file)
+        {
+            var uploadResult = new VideoUploadResult();
+
+            if (file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+                var uploadParams = new VideoUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Folder = "da-net7",
+                    EagerAsync = true,
+                    EagerTransforms = new List<Transformation>()
+                    {
+                        new EagerTransformation().Width(300).Height(300).Crop("fill").AudioCodec("none"),
+                        new EagerTransformation().Width(160).Height(100).Crop("fill").Gravity("south").AudioCodec("none"),
+                    }
+                };
+                uploadResult = await _cloudinary.UploadLargeAsync(uploadParams);
+            }
+
+            return uploadResult;
+        }
+
+        public async Task<DeletionResult> DeleteMediaAsync(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);
+
+            return await _cloudinary.DestroyAsync(deleteParams);
+        }
+    }
+}
