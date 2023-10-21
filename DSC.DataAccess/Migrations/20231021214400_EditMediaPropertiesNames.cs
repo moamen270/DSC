d90cbf624859f0af40a41fc2fc47e39801f6623f
Moamen Mostafa <mmoamen270@gmail.com>
2023-10-22 02:26:18 +0300

    Edit Media Model Properties

---
+++ DSC.DataAccess/Migrations/20231021214400_EditMediaPropertiesNames.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DSC.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditMediaPropertiesNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Media",
+                newName: "Url");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageId",
+                table: "Media",
+                newName: "PublicId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "Media",
+                newName: "ImageUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "PublicId",
+                table: "Media",
+                newName: "ImageId");
+        }
+    }
+}
