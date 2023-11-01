73b12e1a24c4ae2787da80ea337404fafc24d667
Moamen Mostafa <mmoamen270@gmail.com>
2023-11-01 03:31:16 +0200

    Add Public Id For Article Image

---
+++ DSC.DataAccess/Migrations/20231101011446_editArtical.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DSC.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class editArtical : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Articles");
+        }
+    }
+}
--- DSC.Models/Article.cs
+++ DSC.Models/Article.cs
@@ -2,13 +2,15 @@
 {
     public class Article
     {
-            public int Id { get; set; }
-            public string Tittle { get; set; }
-            public DateTime CDate { get; set; } = DateTime.Now;
-            public string Description { get; set; }
-            public string ImgUrl { get; set; }
-            public ArticleType ArticleType { get; set; }
-            public int CategoryId { get; set; }
-            public Category Category { get; set; }
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public DateTime CDate { get; set; } = DateTime.Now;
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+        public string PublicId { get; set; }
+
+        public ArticleType ArticleType { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
