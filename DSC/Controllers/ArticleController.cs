b2b307a0c7323cf690b68df241889962c43dd412
Moamen Mostafa <mmoamen270@gmail.com>
2023-11-01 03:32:51 +0200

    Edit Controller with Upload Image Feature

--- DSC/Controllers/ArticleController.cs
+++ DSC/Controllers/ArticleController.cs
@@ -1,5 +1,7 @@
-﻿using DSC.Models.DTOs;
+﻿using DSC.Models;
+using DSC.Models.DTOs;
 using DSC.Models.Enums;
+using DSC.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,70 +9,88 @@ namespace DSC.Controllers
 {
     public class ArticleController : Controller
     {
-		private readonly IUnitOfWork _unitOfWork;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMediaService _mediaService;
 
-		public ArticleController(IUnitOfWork unitOfWork)
-		{
-			_unitOfWork = unitOfWork;
-		}
+        public ArticleController(IUnitOfWork unitOfWork, IMediaService mediaService)
+        {
+            _unitOfWork = unitOfWork;
+            _mediaService = mediaService;
+        }
 
-
-		public async Task<IActionResult> Index()
-		{
-  /*          ViewBag.Category = await _unitOfWork.Category.GetAllAsync();*/
+        public async Task<IActionResult> Index()
+        {
+            /*          ViewBag.Category = await _unitOfWork.Category.GetAllAsync();*/
             var categories = await _unitOfWork.Category.GetAllAsync();
-			var articles = await _unitOfWork.Article.GetAllAsync();
-			return View(new ArticleDto { Articles = articles, Categories = categories });
-		}
+            var articles = await _unitOfWork.Article.GetAllAsync();
+            return View(new ArticleDto { Articles = articles, Categories = categories });
+        }
+
         [HttpGet]
         public async Task<IActionResult> Details(int id)
         {
-            var article = await _unitOfWork.Article.FirstOrDefaultAsync(a => a.Id == id,includeProperties:c=>c.Category);
+            var article = await _unitOfWork.Article.FirstOrDefaultAsync(a => a.Id == id, includeProperties: c => c.Category);
 
             if (article == null)
-				return NotFound();
+                return NotFound();
 
             return View(article);
         }
 
-
-
         [HttpPost]
-		public async Task<IActionResult> Create(Article article)
-		{
+        public async Task<IActionResult> Create(Article article)
+        {
             if (ModelState.IsValid)
             {
                 await _unitOfWork.Article.AddAsync(article);
                 await _unitOfWork.Save();
-                return RedirectToAction("Index"); 
+                return RedirectToAction("Index");
             }
             return View(article);
-
         }
 
-		[HttpPost]
-		public async Task<IActionResult> Edit(Article article)
-		{
-			if (ModelState.IsValid)
-			{
-				_unitOfWork.Article.Update(article);
-				await _unitOfWork.Save();
-				return RedirectToAction("Index");
-			}
-			return View(article);
-		}
-		[HttpPost]
-		public async Task<IActionResult> Delete(Article article)
-		{
-			if (ModelState.IsValid)
-			{
-				_unitOfWork.Article.Delete(article);
-				await _unitOfWork.Save();
-				return RedirectToAction("Index");
-			}
-			return View(article);
-		}
+        [HttpPost]
+        public async Task<IActionResult> Edit(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                var Article = await _unitOfWork.Article.FirstOrDefaultAsync(m => m.Id == article.Id);
+                if (!string.IsNullOrEmpty(Article.PublicId))
+                {
+                    var DeleteResult = await _mediaService.DeleteMediaAsync(Article.PublicId);
+                    if (DeleteResult.Error is not null)
+                        return RedirectToAction("Index", "Error");
+                }
+                Article.CategoryId = article.CategoryId;
+                Article.Description = article.Description;
+                Article.Tittle = article.Tittle;
+                Article.PublicId = article.PublicId;
+                Article.ArticleType = article.ArticleType;
+                Article.ImgUrl = article.ImgUrl;
+                _unitOfWork.Article.Update(Article);
+                await _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
 
-
-	}
+        [HttpPost]
+        public async Task<IActionResult> Delete(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                var Article = await _unitOfWork.Article.FirstOrDefaultAsync(m => m.Id == article.Id);
+                if (!string.IsNullOrEmpty(Article.PublicId))
+                {
+                    var DeleteResult = await _mediaService.DeleteMediaAsync(Article.PublicId);
+                    if (DeleteResult.Error is not null)
+                        return RedirectToAction("Index", "Error");
+                }
+                _unitOfWork.Article.Delete(Article);
+                await _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+    }
 }
