0d02bc52d402caeff32dfa5f6681d281eb6baa1b
Moamen Mostafa <mmoamen270@gmail.com>
2023-11-22 20:55:18 +0200

    .

---
+++ DSC.Models/DTOs/StudentProfileDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSC.Models.DTOs
+{
+    public class StudentProfileDto
+    {
+        public int Id { get; set; }
+        public string SSN { get; set; }
+        public string SsnImageUrl { get; set; }
+        public string StudentIdImageUrl { get; set; }
+        public string Address { get; set; }
+        public string ImgaeUrl { get; set; }
+        public string DisabilityType { get; set; }
+        public DateTime BithDate { get; set; }
+        public string Department { get; set; }
+        public CollageYear CollageYear { get; set; }
+        public Language Language { get; set; }
+        public StudyType StudyType { get; set; }
+        public Religion Religion { get; set; }
+        public Gender Gender { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<Family> Family { get; set; }
+    }
+}
---
+++ DSC/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using DSC.Models.DTOs;
+
+namespace DSC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<User> _userManager;
+
+        public UserController(IUnitOfWork unitOfWork, UserManager<User> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _unitOfWork.Student.GetAllAsync(includeProperties: s => s.User);
+            return View(users);
+        }
+
+        public async Task<IActionResult> Details(int Id)
+        {
+            var user = await _unitOfWork.Student.FirstOrDefaultAsync(s => s.Id == Id, includeProperties: s => s.User);
+            var Family = await _unitOfWork.Family.GetAllAsync(f => f.StudentId == Id);
+            user.Family = Family.ToList();
+            return View(user);
+        }
+    }
+}
