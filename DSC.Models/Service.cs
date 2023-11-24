47fb8d3314dbeeb969755c34fd567910226a2e27
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-11-24 18:59:42 +0200

    Complete Service

--- DSC.Models/Service.cs
+++ DSC.Models/Service.cs
@@ -6,6 +6,8 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImgUrl { get; set; }
+        public string IconUrl { get; set; }
+
 		public ICollection<Apply> Applies { get; set; }
 	}
 }
